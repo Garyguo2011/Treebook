79bade14db0537ea6fc3ddf29d10f44ff07433cb
Xinran Guo <xguo@berkeley.edu>
2014-12-26 10:39:55 -0800

    Create a reals applicaiton and adds status applicaiton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = 'b1cf7e4cd7128e2bb1789a534afe8de02d271e43f3de2437637bbefb3f3d89d52de6cad4bd5d43344162c8bcf27c51570f753f1ef7176382e8989118d0cd5a63'
